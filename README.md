b831fd3b4c46a532b3e157f8e67033f5e0c6d2f3
Maczomen <maczomen2000@gmail.com>
2023-12-06 18:35:06 +0100

    model for testing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Audio-Emotions
 DNN for predicting emotion from speech
 
-Download Audio Emotions from Kaggle and put it in the /res folder
+Download [Audio Emotions](https://www.kaggle.com/datasets/uldisvalainis/audio-emotions/) from Kaggle and put it in the /res folder
 Create a /spectrograms directory for the results 
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import os
+from datetime import datetime
 
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-max_length = 315700
+import tensorflow as tf
+import keras
+from keras.callbacks import TensorBoard
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+max_length = 230  # 308
+n_mfcc = 40
+num_classes = 7
 
 
 def generate_spectrogram_chart(_input_file, _output_file):
@@ -39,22 +50,29 @@ def generate_spectrogram(_input_file, _output_file):
     plt.close()
 
 
-def generate_padded_spectrogram(input_file, output_file, max_length):
+# 1311  -     / 426
+def generate_padded_spectrogram(input_file, output_file, max_length, save_image=False):
     # Load the audio file
     y, sr = librosa.load(input_file)
+    y, _ = librosa.effects.trim(y)
 
     # Compute the spectrogram
-    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+    #D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+    D = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
 
     # Pad or truncate the spectrogram to the desired length
-    padded_D = pad_sequences([D.T], maxlen=max_length, padding='post', truncating='post', dtype='float32')[0].T
+    padded_D = tf.keras.utils.pad_sequences([D.T], maxlen=max_length, padding='post', truncating='post', dtype='float32', value=0)[0].T
 
     # Plot the spectrogram without labels and margins
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(padded_D, y_axis='log', x_axis=None)
-    plt.axis('off')  # Turn off axis labels
-    plt.savefig(output_file, bbox_inches='tight', pad_inches=0)
-    plt.close()
+    if save_image:
+        plt.ioff()
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(padded_D, y_axis='log', x_axis=None)
+        plt.axis('off')  # Turn off axis labels
+        plt.savefig(output_file, bbox_inches='tight', pad_inches=0)
+        plt.close()
+
+    return padded_D
 
 
 def get_size(_input_file):
@@ -69,6 +87,9 @@ def get_size(_input_file):
 def generate_all_spectrograms():
     input_root = 'res/AudioEmotions/Emotions'
     output_root = 'spectrograms'
+
+    features = []
+    labels = []
 
     # Iterate over all emotion folders
     for emotion in os.listdir(input_root):
@@ -86,12 +107,22 @@ def generate_all_spectrograms():
                 if audio_file.endswith('.wav'):  # You can modify this condition based on your audio file format
                     audio_file_path = os.path.join(emotion_path, audio_file)
                     output_file_path = os.path.join(output_dir, os.path.splitext(audio_file)[0] + '.png')
-                    generate_padded_spectrogram(audio_file_path, output_file_path, max_length)
+                    feature = generate_padded_spectrogram(audio_file_path, output_file_path, max_length)
+                    features.append(feature)
+                    labels.append(emotion)
+
+    label_encoder = LabelEncoder()
+    encoded_labels = label_encoder.fit_transform(labels)
+    labels_one_hot = keras.utils.to_categorical(encoded_labels, num_classes)
+
+    np.savez('spectrograms/spectrogram_data.npz', labels=labels_one_hot, features=np.array(features))
+    return np.array(features), labels_one_hot
 
 
 def get_max_size():
     input_root = 'res/AudioEmotions/Emotions'
-    max_size = 0
+    max_size = (0, 0)
+    lengths = []
 
     # Iterate over all emotion folders
     for emotion in os.listdir(input_root):
@@ -104,12 +135,77 @@ def get_max_size():
             for audio_file in os.listdir(emotion_path):
                 if audio_file.endswith('.wav'):  # You can modify this condition based on your audio file format
                     audio_file_path = os.path.join(emotion_path, audio_file)
-                    size = get_size(audio_file_path)
-                    if size > max_size:
-                        max_size = size
+                    y, sr = librosa.load(audio_file_path)
+                    y, _ = librosa.effects.trim(y)
+                    D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+                    size = D.shape
+
+                    lengths.append(size[1])
+                    if size[1] > max_size[1]:
+                        max_size = (size[0], size[1])
+
+    print("Total Spectrograms:", len(lengths))
+    print("Min length:", min(lengths))
+    print("Max length:", max(lengths))
+    print("Mean length:", np.mean(lengths))
+    print("Median length:", np.median(lengths))
+
+    plt.figure(figsize=(10, 5))
+    plt.hist(lengths, bins=20, color='blue', edgecolor='black', alpha=0.7)
+    plt.title('Histogram of Spectrogram lengths')
+    plt.xlabel('length')
+    plt.ylabel('Frequency')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+
     return max_size
 
+def load_spectrogram_data():
+    loaded_data = np.load('spectrograms/spectrogram_data.npz')
+
+    # Access the arrays using the keys provided during saving
+    features = loaded_data['features']
+    labels = loaded_data['labels']
+
+    return features, labels
 
 if __name__ == "__main__":
-    generate_all_spectrograms()
-# generate_spectrogram_chart("./res/AudioEmotions/Emotions/Sad/OAF_king_sad.wav", "spectrograms/d.png")
+    # get_max_size()
+    if not os.path.exists("spectrograms/spectrogram_data.npz"):
+        X_data, y_data = generate_all_spectrograms()
+    else:
+        X_data, y_data = load_spectrogram_data()
+
+
+    x_train, x_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.1, random_state=42)
+    model = Sequential()
+
+    model.add(LSTM(128, input_shape=X_data.shape[1:], return_sequences=True))
+    model.add(LSTM(64))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Set up TensorBoard callback
+    log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+    # Set up ModelCheckpoint callback
+    checkpoint_path = "model_saves/model_checkpoint.h5"
+    checkpoint_callback = ModelCheckpoint(checkpoint_path, save_best_only=True)
+
+    # Train the model
+    model.fit(x_train, y_train, epochs=20, batch_size=32, validation_split=0.2, callbacks=[tensorboard_callback, checkpoint_callback])
+
+    # Evaluate the model on the test set
+    accuracy = model.evaluate(x_test, y_test)[1]
+    print(f"Test Accuracy: {accuracy * 100}%")
+
+    # Evaluate the model on the test set
+    accuracy = model.evaluate(x_train[0:1000], y_train[0:1000])[1]
+    print(f"Test Accuracy: {accuracy * 100}%")
+
+    # generate_spectrogram_chart("./res/AudioEmotions/Emotions/Sad/OAF_king_sad.wav", "spectrograms/d.png")
